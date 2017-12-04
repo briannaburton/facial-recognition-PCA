212097640153be6ba96bc5a81cfc33877f72332c
Brianna Burton <briannaburton@gmail.com>
2017-12-04 11:38:29 +0100

    work on normalization

--- normalization.m
+++ normalization.m
@@ -7,7 +7,7 @@ clc;
 N = 64;
 
 %Define the location of the faces_features folder and the file format
-dname = '/Users/briannaburton/Documents/Masters/facial-recognition-PCA/faces_features';
+dname = '../faces_features';
 files = dir(cat(2,dname,'/*.txt'));
 
 %Define an average vector F to store the average locations of the facial
@@ -16,47 +16,69 @@ f1 = importdata(cat(2,dname,'/',files(1).name));
 f1 = cat(2, f1, ones(size (f1, 1), 1));
 av_F = f1;
 
+fiP = [13, 20, 1; 50, 20, 1; 34, 34, 1; 16, 50, 1; 48, 50, 1]; %predetermined location
+A = av_F \ fiP; %Find transformation matrix
+
 %For a transformation fiP = Afi + b, where A and b are unknown: 
 %fi*A = fiP where A = [a11, a12, 0; a21, a22, 0; b1, b2, 1], fi is the
 %original fi padded by a column of ones in column 3, and fiP is defined
 %below. A is found using SVD (least squares)
 
-fiP = [13, 20, 1; 50, 20, 1; 34, 34, 1; 16, 50, 1; 48, 50, 1]; %predetermined location
-A = av_F \ fiP; %Find transformation matrix
-
-
-%Once the transformation A is found, apply it on every face image
-new_av_F = zeros([5,3]);
-for i=1:length(files)
-    cfile = files(i).name;
-    f1 = importdata(cat(2,dname,'/',cfile));
-    f1 = cat(2, f1, ones(size (f1, 1), 1));
-    fiP = f1 * A;
-    new_av_F = new_av_F + fiP;
-end
-new_av_F = new_av_F/(length(files));
-
-iterations = 0;
-
-%In the loop, determine new_av_F and compare it to the previous av_F. Stop
-%if less than a threshold.
 stop_cond = (0.01 * ones([5,2]));
 stop_cond = cat(2, stop_cond, zeros(size (stop_cond, 1), 1));
+av_F2 = zeros(5,3);
 
-while (abs(av_F-new_av_F) >= stop_cond)
-    av_F = new_av_F;
+%Once the transformation A is found, apply it on every face image
+while (abs(av_F-av_F2) >= stop_cond)
+    new_av_F = zeros(5,3);
+    iterations = 1;
     fiP = [13, 20, 1; 50, 20, 1; 34, 34, 1; 16, 50, 1; 48, 50, 1]; %predetermined location
-    A = av_F \ fiP; %Find transformation matrix
+    A = av_F \ fiP;
+    for i=2:length(files)
+        cfile = files(i).name;
+        f1 = importdata(cat(2,dname,'/',cfile));
+        f1 = cat(2, f1, ones(size (f1, 1), 1));
+        A = f1 \ av_F;
+        new_av_F = f1 * A;
+        av_F = (av_F*(iterations-1))/iterations + new_av_F/iterations;
+        iterations = iterations + 1;
+    end
 
-    %Apply A to every image and find the mean transformation
-    new_av_F = zeros([5,3]);
+    new_av_F2 = zeros(5,3);
+    iterations2 = 1;
+    av_F2 = av_F;
+
     for i=1:length(files)
         cfile = files(i).name;
         f1 = importdata(cat(2,dname,'/',cfile));
         f1 = cat(2, f1, ones(size (f1, 1), 1));
-        fiP = f1 * A;
-        new_av_F = new_av_F + fiP;
+        A = f1 \ av_F2;
+        new_av_F2 = f1 * A;
+        av_F2 = (av_F2*(iterations2-1))/iterations2 + new_av_F2/iterations2;
+        iterations2 = iterations2 + 1;
     end
-    new_av_F = new_av_F/(length(files))
-    iterations = iterations + 1
 end
+
+% 
+% %In the loop, determine new_av_F and compare it to the previous av_F. Stop
+% %if less than a threshold.
+% stop_cond = (0.01 * ones([5,2]));
+% stop_cond = cat(2, stop_cond, zeros(size (stop_cond, 1), 1));
+% 
+% while (abs(av_F-new_av_F) >= stop_cond)
+%     av_F = new_av_F;
+%     fiP = [13, 20, 1; 50, 20, 1; 34, 34, 1; 16, 50, 1; 48, 50, 1]; %predetermined location
+%     A = av_F \ fiP; %Find transformation matrix
+% 
+%     %Apply A to every image and find the mean transformation
+%     new_av_F = zeros([5,3]);
+%     for i=1:length(files)
+%         cfile = files(i).name;
+%         f1 = importdata(cat(2,dname,'/',cfile));
+%         f1 = cat(2, f1, ones(size (f1, 1), 1));
+%         fiP = f1 * A;
+%         new_av_F = new_av_F + fiP;
+%     end
+%     new_av_F = new_av_F/(length(files))
+%     iterations = iterations + 1
+% end
